1060836f1560c1c2599204e7cff5bb363a8794ff
gmiak <kayss.g@gmail.com>
2023-03-10 00:12:54 +0100

    Navbar implemented and home page init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Divider } from 'primereact/divider';
+import NavBarApp from './modules/nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBarApp />
+      <div>
+        <img src='assets/logo.jpg' alt='logo' />
+      </div>
+      <Divider />
+
+      <footer>
+        <div className='footer'>
+          <p>Get connected with us on social networks:</p>
+          <a href='https://www.facebook.com/mr.gmiak.dv/'><i className="pi pi-facebook"> </i></a>
+          <a href='https://www.instagram.com/gmiak.dv/'><i className="pi pi-instagram"> </i></a>
+          <a href='https://twitter.com/gmiak_dv'><i className="pi pi-twitter"> </i></a>
+          <a href='https://github.com//gmiak'><i className="pi pi-github"> </i></a>
+          <p>Fuggumus Fenster- & Gebäudereinigung</p>
+          <p>0177-7950855 0228-94738479</p>
+          <p>Schweidnitzer Weg 13 53119 Bonn - gianny-musila@fuggmus.de</p>
+          <p className='copyright'>2023 &copy; gmiak.dv, All rights reserved.</p>
+        </div>
+      </footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
