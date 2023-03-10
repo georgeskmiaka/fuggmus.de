f60a9740914d1d066b9bd659d8c8b1836f5721c1
gmiak <kayss.g@gmail.com>
2023-03-10 01:13:01 +0100

    test deployment

--- src/App.js
+++ src/App.js
@@ -12,19 +12,7 @@ function App() {
       </div>
       <Divider />
 
-      <footer>
-        <div className='footer'>
-          <p>Get connected with us on social networks:</p>
-          <a href='https://www.facebook.com/mr.gmiak.dv/'><i className="pi pi-facebook"> </i></a>
-          <a href='https://www.instagram.com/gmiak.dv/'><i className="pi pi-instagram"> </i></a>
-          <a href='https://twitter.com/gmiak_dv'><i className="pi pi-twitter"> </i></a>
-          <a href='https://github.com//gmiak'><i className="pi pi-github"> </i></a>
-          <p>Fuggumus Fenster- & Gebäudereinigung</p>
-          <p>0177-7950855 0228-94738479</p>
-          <p>Schweidnitzer Weg 13 53119 Bonn - gianny-musila@fuggmus.de</p>
-          <p className='copyright'>2023 &copy; gmiak.dv, All rights reserved.</p>
-        </div>
-      </footer>
+      
     </div>
   );
 }
